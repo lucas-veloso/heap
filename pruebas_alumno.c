57904cbf0f2e28673258dc29986c44eb0cfa2832
gonzadiz <gonzadma@gmail.com>
2017-05-24 20:33:20 -0300

    archivo pruebas_alumno.c creado

---
+++ pruebas_alumno.c
@@ -0,0 +1,14 @@
+#include "testing.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+
+/* ******************************************************************
+ *                        PRUEBAS UNITARIAS
+ * *****************************************************************/
+
+void pruebas_heap_alumno (){
+}
