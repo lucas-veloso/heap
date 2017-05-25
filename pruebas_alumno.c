611d99f3244afc98ab0015c0e5a4e750a293caee
gonzadiz <gonzadma@gmail.com>
2017-05-24 23:31:09 -0300

    creando pruebas heap_crear_arr

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -10,6 +10,10 @@ int cmp (const void* a,const void *b){
 	return strcmp(a,b);
 }
 
+typedef struct pruebas{
+	void* elementos;
+}pruebas_t;
+
 
 /* ******************************************************************
  *                        PRUEBAS UNITARIAS
@@ -70,7 +74,31 @@ static void pruebas_heap_encolar_desencolar(){
 	print_test("Heap destruido correctamente",true);
 }
 
+static void pruebas_crear_arr_heapify(){
+	void** elementos = malloc(sizeof(void*) * 4);
+	char* nombres[] = {"Gonzalo","Lucia","Ana","Sebastian"};
+	elementos[0]=nombres[0];
+	elementos[1]=nombres[1];
+	elementos[2]=nombres[2];
+	elementos[3]=nombres[3];
+	heap_t* heap = heap_crear_arr(elementos,4,cmp);
+	print_test("Heap creado correctamente", heap != NULL);
+	print_test("Heap cantidad es 4", heap_cantidad(heap) == 4);
+	print_test("Heap ver maximo es Sebastian", heap_ver_max(heap) == elementos[3]);
+	print_test("Heap desencolar", heap_desencolar(heap) == elementos[3]);
+	print_test("Heap ver maximo es Lucia", heap_ver_max(heap) == elementos[1]);
+	print_test("Heap cantidad es 3",heap_cantidad(heap) == 3);
+	print_test("Heap desencolar", heap_desencolar(heap) == elementos[1]);
+	print_test("Heap ver maximo es Gonzalo",heap_ver_max(heap) == elementos[0]);
+	print_test("Heap cantidad es 2", heap_cantidad(heap) == 2);
+
+	free(elementos);
+	heap_destruir(heap,NULL);
+	print_test("Heap destruido correctamente",true);
+}
+
 void pruebas_heap_alumno (){
 	pruebas_crear_heap_vacio();
 	pruebas_heap_encolar_desencolar();
+	pruebas_crear_arr_heapify();
 }
